bdf6085277488e9ca8752a9b8a79ce7c8d781f8b
rscottlundgren <rscottlundgren@gmail.com>
2020-07-30 09:52:39 -0400

    Initial Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+	return (
+		<React.Fragment>
+			<h1>About</h1>
+			<p>
+				This is the Todo List application I made following the Traversy
+				Media tutorial of React.
+			</p>
+		</React.Fragment>
+	)
+}
+
+export default About
